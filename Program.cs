ba356f358db917d852ae264abddfa077c61e0a6c
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2025-10-06 17:05:43 +0600

    updated

--- Program.cs
+++ Program.cs
@@ -4,4 +4,25 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+// REST PI => GET, POST, PUT, DELETE
+
+app.MapGet("/", () => "API is working fine");
+
+app.MapGet("/hello", () => "Hello World!");
+
+app.MapPost("/hello", () =>
+{
+    return "Post method Hello World!";
+});
+
+app.MapPut("/hello", () =>
+{
+    return "Put method Hello World!";
+});
+
+app.MapDelete("/hello", () =>
+{
+    return "Delete method Hello World!";
+});
+
 app.Run();
